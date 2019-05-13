ebd54f43853abbc5acb7a6b49190f60e51115142
sderis03 <sderis@citadel.edu>
2019-05-13 17:42:30 -0400

    Official start

---
+++ Main.java
@@ -0,0 +1,14 @@
+package eightpuzzle.csis604.util;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		EightPuzzle game = new EightPuzzle(new PriorityQueueHeap(), new int[]{0,1,2,3,4,5,6,7,8});
+		
+		System.out.println();
+	}
+
+}
